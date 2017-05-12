29647425d1ee8783383bbcb771aab841dd2f59e8
giuglio fontana <giuglio fontana>
2017-05-13 00:26:39 +0100

    pa market added

--- script/db_init.sql
+++ script/db_init.sql
@@ -8,3 +8,4 @@ db.date.insert({wrk_date:"02/21/2017"})
 use ordgen
 db.mi_date.insert({wrk_date:"03/07/2017"})
 db.nyse_date.insert({wrk_date:"03/07/2017"})
+db.pa_date.insert({wrk_date:"05/12/2017"})
